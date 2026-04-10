01388d316a12b6b36c640e13f26127be4db8b661
Arijit Ajay Kumar <2023kucp1071@iiitkota.ac.in>
2026-04-10 23:35:27 +0530

    force swagger AWS IP

--- job-service/src/main/java/com/platform/job_service/JobServiceApplication.java
+++ job-service/src/main/java/com/platform/job_service/JobServiceApplication.java
@@ -1,9 +1,12 @@
 package com.platform.job_service;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(servers = {@Server(url = "http://13.61.182.103:8080", description = "AWS Gateway")})
 public class JobServiceApplication {
 
 	public static void main(String[] args) {
--- recruiter-service/src/main/java/com/platform/recruiter_service/RecruiterServiceApplication.java
+++ recruiter-service/src/main/java/com/platform/recruiter_service/RecruiterServiceApplication.java
@@ -3,9 +3,13 @@ package com.platform.recruiter_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+
+@OpenAPIDefinition(servers = {@Server(url = "http://13.61.182.103:8080", description = "AWS Gateway")})
 public class RecruiterServiceApplication {
 
 	public static void main(String[] args) {
